5a6c3ce50c8124cc8b067fc4bc9135c0e2ae0735
Francisco Zuniga Segura <francisco.zuniga@Francisco-Zuniga-MacBook-Pro.local>
2021-06-26 17:13:39 -0600

    add code

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramdevMst
+export POSTGRES_PASSWORD=sTZR3n7gdExTccB
+export POSTGRES_HOST=udagramdev.ciirq45llfoj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdev
+export AWS_BUCKET=udagram1413977-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hola
+export URL=http://localhost:8100
